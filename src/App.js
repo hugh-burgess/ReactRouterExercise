3ed153e3d217b83ca5079b23c5fbf98ca74e1c80
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-04-09 11:48:31 +0200

    Nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <div className="App">Rick & Morty</div>;
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <Navigation />
+        </header>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navigation() {
+  return (
+    <nav className="Navigation">
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+
+      <NavLink exact to="/about">
+        About
+      </NavLink>
+
+      <NavLink exact to="/hobbies">
+        Hobbies
+      </NavLink>
+
+      <NavLink exact to="/contact">
+        Contact
+      </NavLink>
+    </nav>
+  );
+}
